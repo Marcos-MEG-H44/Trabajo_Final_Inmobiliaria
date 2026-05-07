dde5dde27900dcf07f64676aeb848d99e28543e0
Mateo Cluchinsky <mcluchinsky@gmail.com>
2026-05-07 03:08:00 -0300

    Reestructurar los métodos DAO de usuario para mejorar la seguridad y la coherencia

--- dao/dao_usuarios.php
+++ dao/dao_usuarios.php
@@ -3,26 +3,42 @@ require_once(__DIR__ . "/../db/db_connection.php");
 
 class dao_usuarios {
     public static function create($username, $password, $id_persona, $id_perfil) {
-        $sql = "INSERT INTO usuarios (username, password, id_persona, id_perfil) 
+        
+        $username = DBConnection::escape($username);
+        $password = DBConnection::escape($password);
+        $id_persona = (int)$id_persona;
+        $id_perfil = (int)$id_perfil;
+        
+        $sql = "INSERT INTO usuarios (username, password_hash, id_persona, id_perfil) 
                 VALUES ('$username', '$password', $id_persona, $id_perfil)";
         return DBConnection::query($sql);
     }
 
     public static function read($id_usuario) {
-        $sql = "SELECT * FROM usuarios WHERE id_usuario = $id_usuario";
+        $id = (int)$id_usuario; // Casting a entero por seguridad
+        
+        $sql = "SELECT * FROM usuarios WHERE id_usuario = $id";
         return DBConnection::query($sql);
     }
 
     public static function update($id_usuario, $username, $password, $id_persona, $id_perfil) {
+        $id = (int)$id_usuario;
+        $username = DBConnection::escape($username);
+        $password = DBConnection::escape($password);
+        $id_persona = (int)$id_persona;
+        $id_perfil = (int)$id_perfil;
+        
         $sql = "UPDATE usuarios 
-                SET username='$username', password='$password', 
+                SET username='$username', password_hash='$password', 
                     id_persona=$id_persona, id_perfil=$id_perfil 
-                WHERE id_usuario=$id_usuario";
+                WHERE id_usuario=$id";
         return DBConnection::query($sql);
     }
 
     public static function delete($id_usuario) {
-        $sql = "DELETE FROM usuarios WHERE id_usuario=$id_usuario";
+        $id = (int)$id_usuario; //Mismo casting a entero
+        
+        $sql = "DELETE FROM usuarios WHERE id_usuario=$id";
         return DBConnection::query($sql);
     }
 }
