cfbf1d84054de656a85d1e667948951af45e8c1e
Mateo Cluchinsky <mcluchinsky@gmail.com>
2026-05-07 02:48:43 -0300

    Sanear los datos de entrada en los métodos DAO

--- dao/dao_personas.php
+++ dao/dao_personas.php
@@ -3,6 +3,12 @@ require_once(__DIR__ . "/../db/db_connection.php");
 
 class dao_personas {
     public static function create($nombre, $apellido, $id_tipo_doc, $nro_documento, $email) {
+        // Limpia los datos antes de usarlos
+        $n = DBConnection::escape($nombre);
+        $a = DBConnection::escape($apellido);
+        $doc = DBConnection::escape($nro_documento);
+        $e = DBConnection::escape($email);
+        
         $sql = "INSERT INTO personas (nombre, apellido, id_tipo_doc, nro_documento, email) 
                 VALUES ('$nombre', '$apellido', $id_tipo_doc, '$nro_documento', '$email')";
         return DBConnection::query($sql);
@@ -14,15 +20,26 @@ class dao_personas {
     }
 
     public static function update($id_persona, $nombre, $apellido, $id_tipo_doc, $nro_documento, $email) {
+        
+        $n = DBConnection::escape($nombre);
+        $a = DBConnection::escape($apellido);
+        $doc = DBConnection::escape($nro_documento);
+        $e = DBConnection::escape($email);
+        // 2. Asegura que los IDs sean números
+        $id_p = (int)$id_persona;
+        $id_td = (int)$id_tipo_doc;
+        
         $sql = "UPDATE personas 
-                SET nombre='$nombre', apellido='$apellido', id_tipo_doc=$id_tipo_doc, 
-                    nro_documento='$nro_documento', email='$email' 
-                WHERE id_persona=$id_persona";
+                SET nombre='$n', apellido='$a', id_tipo_doc=$id_td, 
+                    nro_documento='$doc', email='$e' 
+                WHERE id_persona=$id_p";
         return DBConnection::query($sql);
     }
 
     public static function delete($id_persona) {
-        $sql = "DELETE FROM personas WHERE id_persona=$id_persona";
+
+        $id = (int)$id_persona;
+        $sql = "DELETE FROM personas WHERE id_persona=$id";
         return DBConnection::query($sql);
     }
 }
